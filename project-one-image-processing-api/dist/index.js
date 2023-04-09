b3e5fbbc617dc954eb5ac8affe4d2b8fbe00dc68
Vishnu Sujeesh <vsujeesh@its.jnj.com>
2023-04-10 00:21:27 +0800

    Express app hello world up and running

--- project-one-image-processing-api/dist/index.js
+++ project-one-image-processing-api/dist/index.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log("new project initialised");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const imageResizerApp = (0, express_1.default)();
+const port = 3000;
+imageResizerApp.get('/', (req, res) => {
+    res.send("Hello World");
+});
+imageResizerApp.listen(port, () => {
+    console.log(`Image resizer app is up and running, listening on port ${port}`);
+});
--- project-one-image-processing-api/src/index.ts
+++ project-one-image-processing-api/src/index.ts
@@ -1,1 +1,12 @@
-console.log("new project initialised");
+import express from "express"
+
+const imageResizerApp: express.Express = express()
+const port: number = 3000
+
+imageResizerApp.get('/', (req, res)=>{
+    res.send("Hello World")
+})
+
+imageResizerApp.listen(port, ()=>{
+    console.log(`Image resizer app is up and running, listening on port ${port}`)
+})
