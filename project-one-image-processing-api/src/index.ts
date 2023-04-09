c1667f7e7c54ee51cee91fe70842f036c6e51b40
Vishnu Sujeesh <vsujeesh@its.jnj.com>
2023-04-10 00:32:27 +0800

    running express server with folder for assets

--- project-one-image-processing-api/src/index.ts
+++ project-one-image-processing-api/src/index.ts
@@ -1,12 +1,12 @@
-import express from "express"
+import express from "express";
 
-const imageResizerApp: express.Express = express()
-const port: number = 3000
+const imageResizerApp: express.Express = express();
+const port: number = 3000;
 
-imageResizerApp.get('/', (req, res)=>{
-    res.send("Hello World")
-})
+imageResizerApp.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-imageResizerApp.listen(port, ()=>{
-    console.log(`Image resizer app is up and running, listening on port ${port}`)
-})
+imageResizerApp.listen(port, () => {
+  console.log(`Image resizer app is up and running, listening on port ${port}`);
+});
